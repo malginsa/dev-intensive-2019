46dd7f6d7daca3063048467a035431cabaeecda2
malginsa <malginsa@gmail.com>
2019-07-11 14:50:00 +0300

    humanizeDiff added

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -2,14 +2,29 @@ package ru.skillbranch.devintensive.extensions
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
 const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
 const val HOUR = 60 * MINUTE
 const val DAY = 24 * HOUR
 
-fun Date.humanizeDiff(date:Date = Date()): String {
-    return "когда-то"
+fun Date.humanizeDiff(compareTo: Date = Date()): String {
+    val diff = compareTo.time - this.time
+    val isInThePast = compareTo.time > this.time
+    val result = when (diff.absoluteValue) {
+        in 0 * SECOND..1 * SECOND -> return "только что"
+        in 2 * SECOND..45 * SECOND -> "несколько секунд"
+        in 46 * SECOND..75 * SECOND -> "минуту"
+        in 76 * SECOND..45 * MINUTE -> TimeUnits.MINUTE.plural((1.0 * diff / MINUTE).roundToInt())
+        in 46 * SECOND..75 * MINUTE -> "час"
+        in 76 * MINUTE..22 * HOUR -> TimeUnits.HOUR.plural((1.0 * diff / HOUR).roundToInt())
+        in 23 * HOUR..26 * HOUR -> "день"
+        in 27 * HOUR..360 * DAY -> TimeUnits.DAY.plural((1.0 * diff / DAY).roundToInt())
+        else -> {return if(isInThePast) "более года назад" else "более чем через год"}
+    }
+    return if (isInThePast) result + " назад" else "через " + result
 }
 
 fun Date.format(pattern: String = "HH:mm:ss dd:MM:yy"): String {
@@ -35,8 +50,9 @@ enum class TimeUnits {
     DAY;
 
     fun plural(value: Int): String {
-        val normalized = if (value>20) value%10 else value
-        var result = "$value "
+        val absoluteValue = value.absoluteValue
+        val normalized = if (value>20) (absoluteValue%10) else absoluteValue
+        var result = "$absoluteValue "
         result += when (this) {
             SECOND -> when(normalized) {
                 1 -> "секунду"
--- app/src/test/java/ru/skillbranch/devintensive/extensions/DateTest.kt
+++ app/src/test/java/ru/skillbranch/devintensive/extensions/DateTest.kt
@@ -6,7 +6,8 @@ import java.util.*
 
 class DateTest {
 
-    val date = Date(1_000_000_000_000)
+    private val date = Date(1_000_000_000_000)
+    private val current = Date()
 
     @Test
     fun format_full(){
@@ -52,5 +53,95 @@ class DateTest {
     fun timeUnits_day(){
         val plural = TimeUnits.DAY.plural(222)
         assertEquals("222 дня", plural)
+    }
+
+    @Test
+    fun humanizeDiff_justNow(){
+        val actual = current.humanizeDiff()
+        assertEquals("только что", actual)
+    }
+
+    @Test
+    fun humanizeDiff_severalSecondsAgo(){
+        val actual = current.add(-21, TimeUnits.SECOND).humanizeDiff()
+        assertEquals("несколько секунд назад", actual)
+    }
+
+    @Test
+    fun humanizeDiff_OneMinuteAgo(){
+        val actual = current.add(-65, TimeUnits.SECOND).humanizeDiff()
+        assertEquals("минуту назад", actual)
+    }
+
+    @Test
+    fun humanizeDiff_SomeMinutesAgo(){
+        val actual = current.add(-18, TimeUnits.MINUTE).humanizeDiff()
+        assertEquals("18 минут назад", actual)
+    }
+
+    @Test
+    fun humanizeDiff_OneHourAgo(){
+        val actual = current.add(-46, TimeUnits.MINUTE).humanizeDiff()
+        assertEquals("час назад", actual)
+    }
+
+    @Test
+    fun humanizeDiff_SomeHoursAgo(){
+        val actual = current.add(-11, TimeUnits.HOUR).humanizeDiff()
+        assertEquals("11 часов назад", actual)
+    }
+
+    @Test
+    fun humanizeDiff_OneDayAgo(){
+        val actual = current.add(-25, TimeUnits.HOUR).humanizeDiff()
+        assertEquals("день назад", actual)
+    }
+
+    @Test
+    fun humanizeDiff_SomeDaysAgo(){
+        val actual = current.add(-117, TimeUnits.DAY).humanizeDiff()
+        assertEquals("117 дней назад", actual)
+    }
+
+    @Test
+    fun humanizeDiff_MoreThanOneYearAgo(){
+        val actual = current.add(-365, TimeUnits.DAY).humanizeDiff()
+        assertEquals("более года назад", actual)
+    }
+
+    @Test
+    fun humanizeDiff_1(){
+        val actual = current.add(-2, TimeUnits.HOUR).humanizeDiff()
+        assertEquals("2 часа назад", actual)
+    }
+
+    @Test
+    fun humanizeDiff_2(){
+        val actual = current.add(-5, TimeUnits.DAY).humanizeDiff()
+        assertEquals("5 дней назад", actual)
+    }
+
+    @Test
+    fun humanizeDiff_3(){
+        val actual = current.add(2, TimeUnits.MINUTE).humanizeDiff()
+        assertEquals("через 2 минуты", actual)
+    }
+
+    @Test
+    fun humanizeDiff_4(){
+        val actual = current.add(7, TimeUnits.DAY).humanizeDiff()
+        assertEquals("через 7 дней", actual)
+    }
+
+    @Test
+    fun humanizeDiff_5(){
+        val actual = current.add(-400, TimeUnits.DAY).humanizeDiff()
+        assertEquals("более года назад", actual)
+    }
+
+    @Test
+    fun humanizeDiff_6(){
+        val actual = current.add(400, TimeUnits.DAY).humanizeDiff()
+        assertEquals("более чем через год", actual)
     }
 }
